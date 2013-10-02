472be4356401a9767a713e2f4c84ea1806aed41f
OKAZAKI Takurou <okazaki@isola.co.jp>
2013-10-02 15:42:29 +0900

    rails new . -T --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nessie::Application.config.secret_key_base = '019a90eaf756ad3fe8aabd145e1b1c21a352e41df55977760074ddfab878d15f114f63705afcec072ab322253dc3b75de5b3665e5a23131067bdadc0082625f8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nessie::Application.config.session_store :cookie_store, key: '_nessie_session'
