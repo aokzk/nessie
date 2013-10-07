d9180aaf32b2d7ba609753397c59f671ce339ba6
OKAZAKI Takurou <okazaki@isola.co.jp>
2013-10-07 15:09:24 +0900

    param.permit moved to each controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Nessie::Application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
 
   root 'home#index'
 
