70d1defc1a43b24fa235dbf974a1e519ed315f5f
OKAZAKI Takurou <okazaki@isola.co.jp>
2013-10-07 13:38:32 +0900

    Add username to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+            :presence => true,
+            :uniqueness => true,
+            :length => { :minimum => 6, :maximum => 24 },
+            :format => { :with => /\A[a-zA-Z\d_\.]+\Z/ }
 end
